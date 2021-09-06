35d0f3294698f380e99224b3a1f6f8748386db38
Gyanam Gogoi <pythoncoder13@gmail.com>
2021-09-06 23:10:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Your posts shown below. Or compose a post.";
+const aboutContent = "Developed by GYANAM";
+const contactContent = "Contact email adress is ggyanam869@gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+
+const postSchema = {
+    title: String,
+    author: String,
+    image: String,
+    content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res) {
+
+    Post.find({}, function(err, posts) {
+        res.render("home", {
+            startingContent: homeStartingContent,
+            posts: posts
+        });
+    });
+});
+
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+    const post = new Post({
+        title: req.body.postTitle,
+        author: req.body.authorName,
+        image: req.body.imageURL,
+        content: req.body.postBody
+    });
+
+
+    post.save(function(err) {
+        if (!err) {
+            res.redirect("/");
+        }
+    });
+});
+
+app.get("/posts/:postId", function(req, res) {
+
+    const requestedPostId = req.params.postId;
+
+    Post.findOne({ _id: requestedPostId }, function(err, post) {
+        res.render("post", {
+            title: post.title,
+            author: post.author,
+            image: post.image,
+            content: post.content
+        });
+    });
+
+});
+
+app.get("/about", function(req, res) {
+    res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function(req, res) {
+    res.render("contact", { contactContent: contactContent });
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server started on port 3000");
+});
